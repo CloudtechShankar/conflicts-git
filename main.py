8c76f791b7d28e9a653a383638c04ca25c970e75
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-10-23 21:54:33 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import boto3
+from flask import Flask, jsonify
+from botocore.exceptions import BotoCoreError, ClientError
+import os
+app = Flask(__name__)
+@app.route('/list-s3-buckets', methods=['GET'])
+def list_s3_buckets():
+    """
+    List all S3 buckets in the AWS account.
+    """
+    s3 = boto3.client('s3')
+    try:
+        response = s3.list_buckets()
+        buckets = [bucket['Name'] for bucket in response.get('Buckets', [])]
+        return jsonify({'buckets': buckets}), 200
+    except (BotoCoreError, ClientError) as e:
+        return jsonify({'error': str(e)}), 500
